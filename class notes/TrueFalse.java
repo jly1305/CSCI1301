19553aff1a9ef380df9dd621eb243ddfb6ad2b11
luke t <luket678@gmail.com>
2023-08-31 11:42:00 -0400

    Add files via upload

---
+++ class notes/TrueFalse.java
@@ -0,0 +1,15 @@
+//A program for demonstrating boolean variables
+public class TrueFalse
+{
+   public static void main(String[] args)
+   {
+      boolean bool;
+      //assigning data type boolean to name 'bool'
+      bool = true;
+      System.out.println(bool);
+      bool = false;
+      System.out.println(bool);
+      
+   }
+}
+   
