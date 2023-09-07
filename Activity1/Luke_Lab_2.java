c56289ce902b3072028c5260da6e08cc5f029eb6
luke t <luket678@gmail.com>
2023-09-07 12:13:47 -0400

    Add files via upload

---
+++ Activity1/Luke_Lab_2.java
@@ -0,0 +1,63 @@
+//This program created by Lke Tierney, for course CSCI 1301, in fall 2023 in 09/07/2023. For lab 2 activity.
+
+public class Luke_Lab_2
+{
+   public static void main(String[] args)
+   {
+    // print welcome statement
+    System.out.println("               Welcome to Lab 2 Activity."
+                   + "\n               ************************* ");
+    //print using escape sequences in one print statement (i kind of did that already)
+    System.out.println("University: GCSU.\t\t\t\t\t\tCollege: J. Whiteney College of Business of Business and Technology \nDean: Dr. Michael Stratton\t\t\tAssociate Dean: Dr.Tanya Goette");
+    
+    System.out.println("\"John said, \'I prefer using double quotes for strings, but some developers like using single quotes for readability.\'\""
+    + "\n however the fil pathe is C:\\Users\\Username\\Documents\\file.txt");
+    
+    //work with variables
+    byte byte_Variable;
+    short short_Variable;
+    long long_Variable;
+    float float_Variable;
+    
+    byte_Variable = 127;
+    short_Variable = 127;
+    long_Variable = 127;
+    float_Variable = 127;
+    
+    System.out.println(byte_Variable);
+    System.out.println(short_Variable);
+    System.out.println(long_Variable);
+    System.out.println(float_Variable);
+    
+    int integer_Variable = 255;
+    double double_Variable = 255;
+    boolean boolean_Variable = true;
+    char Char_Varaibe = 'T';
+    
+    System.out.println(integer_Variable);
+    System.out.println(double_Variable);
+    System.out.println(boolean_Variable);
+    System.out.println('T');
+    
+    //Arithmetic Operators
+    
+    int x, y;
+    x = 11;
+    y = x + 5;
+    
+    System.out.println("x and y values are " + x + " and " + y + ". The sum is " + (x + y ) + ". Subtraction is " + (y - x) + ". Multiplication is " + (x * y) + ". Division is " + (y / x) + " ");
+    
+    double r, a, b;
+    
+    a = 6;
+    b = 5;
+    
+    r = 
+
+
+    
+    
+   }
+}
+      
+      
---
+++ class notes/constantfinal.java
@@ -0,0 +1,10 @@
+// 9.5.2023 
+public class constantfinal
+{
+   public static void main(String[] args)
+   {
+      final double PRICE;
+      PRICE = 19.99;
+      System.out.println("The Price is " + PRICE);
+   }
+}
