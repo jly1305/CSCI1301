09b54f4b5850dfddc641678e702f299b05573d0d
luke t <luket678@gmail.com>
2023-09-19 12:11:53 -0400

    Add files via upload

---
+++ sep19note.java
@@ -0,0 +1,115 @@
+//9/19/2023
+import java.util.Scanner;
+public class sep19note
+{
+   public static void main(String[] args)
+   {
+      //if statements run in a flow chart. if something is true, it will act upon it, but if it is not true, the code will simply go on 
+      /** if (coldOutside)
+      {
+         wearCoat();
+         wearHat();
+         wearGloves();
+      }
+      
+      braces needed when an if statement used more than one line **/
+      
+      // if number 1 is less than number 2, print number 1 
+      int num_1, num_2, num_3;
+      num_1 = 104;
+      num_2 = 105;
+      num_3 = 110;
+      
+      System.out.println("Test #1");
+      if (num_1 < num_2)
+         System.out.println("Number 1 is less than number 2");
+      
+      /**relational operators
+      x > y greater
+      x < y less than
+      x >= y greater or equal to 
+      x =< y less or equal to 
+      x == y if x is equal to y 
+      x != y x not equal to y 
+      **/
+      
+      int height = 70;
+      int height_2 = 69;
+      
+      if (height > height_2)
+      {
+      System.out.println("I am happy");
+      }
+      
+      else
+      {
+      System.out.println("I am sad");
+     }
+      
+      
+     /**nested if statements
+     
+     if (coldOutside)
+     {
+         if (snowing)
+         {
+            wearCoat();
+         }
+         else
+         {
+            wearNothing():
+         }
+      }
+      else
+      }
+         wearShorts();
+      }
+      bro what the fuck **/
+      
+      /** 
+      {
+          if
+          {
+          else if
+          {
+          x
+          {
+          else if
+          {
+          x
+          {
+          else
+          {
+       {
+       **/
+       
+       /** logical operators
+       && and 
+       || or
+       ! not 
+       **/
+       
+       /** 
+       if (!(temperature > 100) )
+         System.out.println("Below the maximum temperature. ");
+         
+         int temperature = 110
+         
+         expression is true !expression is false
+       **/
+       
+       
+      
+    
+     
+    
+      
+      
+      
+     
+     
+      
+     
+         
+   }
+}
