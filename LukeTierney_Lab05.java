e985361acd74250d2e0822c54ae8519b6e69fd34
luke t <luket678@gmail.com>
2023-10-29 12:35:17 -0400

    Add files via upload

--- LukeTierney_Lab05.java
+++ LukeTierney_Lab05.java
@@ -110,17 +110,88 @@ public class LukeTierney_Lab05
       } while (repeat == 'y'); System.out.println("Goodbye!");
 
    //#8
-
+      int sum8 = 0;
+      int count8 = 0;
+      int input8;
+      int min = 2000000000;
+      int max = 0;
 
       do
       {
          System.out.println("Enter a number or type \"-1\" to exit");
+         input8 = keyboard.nextInt();
+         
+         if (input8 == -1)
+         {
+            break;
+         }
+         
+         sum8 += input8;
+         count8++;
+         
+         if (input8 > max)
+         {
+            max = input8;
+         }
+         
+         if (input8 < min)
+         {
+            min = input8;
+         }
+         
 
-
-
-      } while ();
-   
+      } while ( input8 > 0); 
       
+      double average8 = (sum8 / count8);
+      System.out.println("The average of the entered numbers: " + average8 + "\nThe max number is: " + max + "\nThe min number is: " + min);
+   
+   //#9 
+
+      int number9;
+
+      for(number9 = 2; number9 <= 50; number9 = number9 + 2)
+      {
+         if (number9 % 2 == 0)
+         {
+            System.out.print(number9 + " ");
+         }
+         else
+         {
+            break;
+         }
+      }  
+
+   //#10
+
+      //declare variables here
+      int userInput10;
+      int count10 = 0;
+      double average10;
+      int min10 = 2000000000;
+      int max10 = 0;
+      int userInputNumber10;
+      int sum10 = 0;
+      int breaker10;
+      int integers10;
+      int inputAmount10;
+      int inputAmountSpecifier = 0;
+
+       /*
+       do this shit later because i suck
+       //start do while sequence
+       do 
+       {
+         System.out.println("How many numbers do you want to enter? ");
+         
+
+         
+         
+       }
+       */
+
+   //#11
+       
+   
       
    }
 }
